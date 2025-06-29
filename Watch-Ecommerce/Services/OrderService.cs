6904d368711b99e682f584fa45b227bcb4e3bd84
SaraHussein4 <saraomran12002@gmail.com>
2025-06-29 20:10:24 +0300

    Order Service

---
+++ Watch-Ecommerce/Services/OrderService.cs
@@ -0,0 +1,61 @@
+﻿using ECommerce.Core.model;
+using Watch_Ecommerce.DTOs.Order;
+using Watch_EcommerceBl.Interfaces;
+using Watch_EcommerceDAL.Models;
+
+namespace Watch_Ecommerce.Services
+{
+    public class OrderService
+    {
+        private readonly ICartRepository _cartRepository;
+        private readonly TikrContext _context;
+        public OrderService(ICartRepository cartRepository , TikrContext tikrContext)
+        {
+            _cartRepository = cartRepository;
+            _context = tikrContext;
+        }
+        public async Task<Order?> CreateOrderAsync(string userId, int deliveryMethodId, OrderAddressDto dto)
+        {
+            var basket = await _cartRepository.GetBasketAsync(userId);
+            if (basket == null || !basket.Items.Any())
+                return null;
+
+            var order = new Order
+            {
+                UserId = userId,
+                DeliveryMethodId = deliveryMethodId,
+                Status = "Pending",
+                Date = DateTime.Now,
+                Amount = basket.Items.Sum(i => i.Price * i.Quantity),
+
+                OrderAddress = new OrderAddress
+                {
+                    FirstName = dto.FirstName,
+                    LastName = dto.LastName,
+                    City = dto.City,
+                    Street = dto.Street,
+                    GovernorateId = dto.GovernorateId
+                },
+
+                OrderItems = basket.Items.Select(i => new OrderItem
+                {
+                    ProductId = i.id,
+                    Quantity = i.Quantity,
+                    Price = i.Price,
+                    Amount = (int)(i.Price * i.Quantity)
+                }).ToList()
+            };
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            await _cartRepository.DeleteBasketAsync(userId);
+
+            return order;
+        }
+
+
+
+
+    }
+}
